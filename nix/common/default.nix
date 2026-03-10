87e75188bee5dfc83233c52fb8bc624d5488c28b
Tim Dortmann <git@tdortman.com>
2026-03-10 12:16:28 +0100

    prefer cache.nixos.org whenever possible

--- nix/common/default.nix
+++ nix/common/default.nix
@@ -18,20 +18,17 @@
   ];
 
   nix.settings = {
-    substituters = [
-      "https://cache.nixos.org"
+    extra-substituters = [
       "https://nix-community.cachix.org"
       "https://cache.flox.dev"
     ];
-    trusted-public-keys = [
-      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+    extra-trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "flox-cache-public-1:7F4OyH7ZCnFhcze3fJdfyXYLQw/aV7GEed86nQ7IsOs="
     ];
     auto-optimise-store = true;
     trusted-users = [
-      currentUsername
-      "root"
+      "@wheel"
     ];
     experimental-features = [
       "nix-command"
