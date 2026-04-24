10653629a961ed42cf606f69deaea619632d1ac8
Tim Dortmann <git@tdortman.com>
2026-04-24 20:50:46 +0200

    vencord: 1.14.7 -> 1.14.10

--- nix/overlays/vencord/default.nix
+++ nix/overlays/vencord/default.nix
@@ -1,13 +1,29 @@
 { ... }:
 
 final: prev: {
-  # vencord = prev.vencord.overrideAttrs (oldAttrs: {
-  #   version = "1.14.5";
-  #   src = prev.fetchFromGitHub {
-  #     owner = "Vendicated";
-  #     repo = "Vencord";
-  #     rev = "v${final.vencord.version}";
-  #     sha256 = "sha256-FZ00lhPr4R0Bo8zwBfc/Y8eMfbTcRxjH3YwDBm1NSQk=";
-  #   };
-  # });
+  vencord = prev.vencord.overrideAttrs (
+    finalAttrs: oldAttrs: {
+      version = "1.14.10";
+
+      src = prev.fetchFromGitHub {
+        owner = "Vendicated";
+        repo = "Vencord";
+        rev = "v${finalAttrs.version}";
+        hash = "sha256-+P0FF7PIJ+z0jBMwQM2JR5d1c05E8EOjUI9j7mAWddQ=";
+      };
+
+      pnpmDeps = prev.fetchPnpmDeps {
+        inherit (finalAttrs)
+          pname
+          src
+          patches
+          postPatch
+          ;
+
+        pnpm = prev.pnpm_10;
+        fetcherVersion = 2;
+        hash = "sha256-GiUV2x8i7ewzn66v5wBUq67oNvrxZzOsh5TuQUtpJNQ=";
+      };
+    }
+  );
 }
