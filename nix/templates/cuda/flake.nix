3a8c46e9e983f8a61bcefe4c4b05c092dd44f13c
Tim Dortmann <git@tdortman.com>
2026-05-11 04:53:59 +0200

    update cuda template

--- nix/templates/cuda/flake.nix
+++ nix/templates/cuda/flake.nix
@@ -6,104 +6,131 @@
   outputs =
     { nixpkgs, ... }:
     let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs {
-        system = system;
-        config.allowUnfree = true;
-      };
-
-      cudaPkgs = pkgs.cudaPackages_13_2;
-      llvm = pkgs.llvmPackages_22;
-
-      cudaPackages = with cudaPkgs; [
-        cuda_nvcc
-        cuda_cudart
-        cuda_cccl
-        cuda_cuobjdump
-      ];
-
-      cuda = {
-        arch = "1200";
-        sm_target = "sm_120";
-        path = pkgs.symlinkJoin {
-          name = "cuda-dev-path";
-          paths = cudaPackages;
-        };
-        version = {
-          complete = cudaPkgs.cudaMajorMinorVersion;
-          major = cudaPkgs.cudaMajorVersion;
-          minor = nixpkgs.lib.lists.last (builtins.splitVersion cuda.version.complete);
-        };
-
-      };
-      buildInputs = cudaPackages ++ [
-        pkgs.stdenv.cc.cc.lib
-      ];
-
-      nativeBuildInputs = with pkgs; [
-        llvm.clang-tools
-        llvm.lldb
-        meson
-        uv
-        pkg-config
-
-        cudaPkgs.nsight_systems
-        cudaPkgs.nsight_compute
+      supportedSystems = [
+        "x86_64-linux"
       ];
     in
     {
-      devShells.${system}.default = pkgs.mkShell {
+      devShells = nixpkgs.lib.genAttrs supportedSystems (
+        system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            config.allowUnfree = true;
+          };
 
-        inherit buildInputs nativeBuildInputs;
+          lib = pkgs.lib;
+          cudaPkgs = pkgs.cudaPackages_13_2;
+          llvmPkgs = pkgs.llvmPackages_22;
 
-        LD_LIBRARY_PATH = "${
-          pkgs.lib.makeLibraryPath (buildInputs ++ nativeBuildInputs)
-        }:/run/opengl-driver/lib";
+          cudaToolkit = pkgs.symlinkJoin {
+            name = "cuda-toolkit";
+            paths = with cudaPkgs; [
+              cuda_nvcc
+              cuda_cudart
+              cuda_crt
 
-        shellHook = ''
-              if [ ! -e .clangd ]; then
-                cat > .clangd <<EOF
-          CompileFlags:
-            Compiler: ${cuda.path}/bin/nvcc
-            Add:
-              - -xcuda
-              - --cuda-path=${cuda.path}
-              - -D__INTELLISENSE__
-              - -D__CLANGD__
-              - -I${cuda.path}/include
-              - -I$(pwd)/include
-              - -D__LIBCUDAXX__STD_VER=${cuda.version.major}
-              - -D__CUDACC_VER_MAJOR__=${cuda.version.major}
-              - -D__CUDACC_VER_MINOR__=${cuda.version.minor}
-              - -D__CUDA_ARCH__=${cuda.arch}
-              - --cuda-gpu-arch=${cuda.sm_target}
-              - -D__CUDACC_EXTENDED_LAMBDA__
-            Remove:
-              - -Xcompiler=*
-              - -G
-              - "-arch=*"
-              - "-Xfatbin*"
-              - "-gencode*"
-              - "--generate-code*"
-              - "--generate-line-info"
-              - "--compiler-options*"
-              - "--expt-extended-lambda"
-              - "--expt-relaxed-constexpr"
-              - "-forward-unknown-to-host-compiler"
-              - "-Werror=cross-execution-space-call"
+              cuda_gdb.bin
+              nsight_systems
+              nsight_compute
+            ];
+          };
 
-          Diagnostics:
-            UnusedIncludes: None
-            Suppress:
-              - variadic_device_fn
-              - attributes_not_allowed
-              - undeclared_var_use_suggest
-              - typename_invalid_functionspec
-              - expected_expression
-          EOF
-                echo ".clangd created by flake shellHook"
-              fi
-        '';
-      };
+          cuda = {
+            arch = "1200";
+            smTarget = "sm_120";
+            path = cudaToolkit;
+            version = {
+              complete = cudaPkgs.cudaMajorMinorVersion;
+              major = cudaPkgs.cudaMajorVersion;
+              minor = lib.lists.last (builtins.splitVersion cuda.version.complete);
+            };
+          };
+
+          buildInputs = [
+            cudaToolkit
+            pkgs.stdenv.cc.cc.lib
+            pkgs.xz
+            pkgs.bzip2
+          ];
+
+          nativeBuildInputs = with pkgs; [
+            llvmPkgs.clang-tools
+            llvmPkgs.clang
+            meson
+            uv
+            pkg-config
+            doxygen
+            graphviz
+
+            ninja
+            cmake
+          ];
+        in
+        {
+          default = pkgs.mkShell {
+            inherit buildInputs nativeBuildInputs;
+
+            env = {
+              CPATH = lib.makeIncludePath [ cuda.path ];
+              CUDA_HOME = cuda.path;
+
+              LD_LIBRARY_PATH = "${
+                lib.makeLibraryPath (buildInputs ++ nativeBuildInputs)
+              }:/run/opengl-driver/lib";
+            };
+
+            shellHook = ''
+                  if [ ! -e .clangd ]; then
+                    cat > .clangd <<EOF
+              CompileFlags:
+                Compiler: ${cuda.path}/bin/nvcc
+                Add:
+                  - -std=c++20
+                  - -xcuda
+                  - --cuda-path=${cuda.path}
+                  - -D__INTELLISENSE__
+                  - -D__CLANGD__
+                  - -I${cuda.path}/include
+                  - -I$(pwd)/include
+                  - -I$(pwd)/subprojects/cuco/include
+                  - -I$(pwd)/subprojects/googletest-1.17.0/googletest/include
+                  - -D__LIBCUDAXX__STD_VER=${cuda.version.major}
+                  - -D__CUDACC_VER_MAJOR__=${cuda.version.major}
+                  - -D__CUDACC_VER_MINOR__=${cuda.version.minor}
+                  - -D__CUDA_ARCH__=${cuda.arch}
+                  - --cuda-gpu-arch=${cuda.smTarget}
+                  - -D__CUDACC_EXTENDED_LAMBDA__
+                  - -DPARAM_SWEEP_GROUP
+                Remove:
+                  - -Xcompiler=*
+                  - -G
+                  - "-arch=*"
+                  - "-Xfatbin*"
+                  - "-gencode*"
+                  - "--generate-code*"
+                  - "--generate-line-info"
+                  - "--compiler-options*"
+                  - "--expt-extended-lambda"
+                  - "--expt-relaxed-constexpr"
+                  - "-forward-unknown-to-host-compiler"
+                  - "-Werror=cross-execution-space-call"
+
+              Diagnostics:
+                UnusedIncludes: None
+                Suppress:
+                  - variadic_device_fn
+                  - attributes_not_allowed
+                  - undeclared_var_use_suggest
+                  - typename_invalid_functionspec
+                  - expected_expression
+                  - deduction_guide_target_attr
+              EOF
+                    echo ".clangd created by flake shellHook"
+                  fi
+            '';
+          };
+        }
+      );
     };
 }
