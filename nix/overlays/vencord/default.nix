572b5fbe24f4f7bdcab4508707cd7dc07953aa7f
Tim Dortmann <git@tdortman.com>
2026-06-25 05:25:40 +0200

    fix: pin vencord to 1.14.15

--- nix/overlays/vencord/default.nix
+++ nix/overlays/vencord/default.nix
@@ -1,27 +1,27 @@
 _:
 
-final: _: {
-  # vencord = prev.vencord.overrideAttrs (
-  #   finalAttrs: oldAttrs: {
-  #     version = "1.14.11";
-  #     src = prev.fetchFromGitHub {
-  #       owner = "Vendicated";
-  #       repo = "Vencord";
-  #       rev = "v${finalAttrs.version}";
-  #       hash = "sha256-Ylu1O4zvnVVEXzNQ5j1+Y2X54lVCyqVJLJa1Ngz+7aA=";
-  #     };
-  #     pnpmDeps = prev.fetchPnpmDeps {
-  #       inherit (finalAttrs)
-  #         pname
-  #         src
-  #         patches
-  #         postPatch
-  #         ;
-  #       pnpm = prev.pnpm_10;
-  #       fetcherVersion = 2;
-  #       hash = "sha256-GiUV2x8i7ewzn66v5wBUq67oNvrxZzOsh5TuQUtpJNQ=";
-  #     };
-  #   }
-  # );
-  inherit (final.master) vencord;
+final: prev: {
+  vencord = prev.vencord.overrideAttrs (
+    finalAttrs: oldAttrs: {
+      version = "1.14.15";
+      src = prev.fetchFromGitHub {
+        owner = "Vendicated";
+        repo = "Vencord";
+        rev = "v${finalAttrs.version}";
+        hash = "sha256-jQeLZa1rpKDkzWSpAqOa8snGRKLpv9xf9cwJ6hUwMzA=";
+      };
+      pnpmDeps = prev.fetchPnpmDeps {
+        inherit (finalAttrs)
+          pname
+          src
+          patches
+          postPatch
+          ;
+        pnpm = prev.pnpm_10;
+        fetcherVersion = 3;
+        hash = "sha256-hk1rnNog5xvuIVI0M1ZJ5xrEuk0zcBiYsbROUycdi+A=";
+      };
+    }
+  );
+  # inherit (final.master) vencord;
 }
