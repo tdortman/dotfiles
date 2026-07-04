c69bceb9e0392ded62e48a03539450911cb68f7c
Tim Dortmann <git@tdortman.com>
2026-07-04 04:35:54 +0200

    feat(nixos-laptop): enable fingerprint reader
    Turn on the fingerprint module and add a host overlay for the
    `elanmoc2` `libfprint` fork required by this laptop hardware.

--- nix/systems/x86_64-linux/nixos-laptop/default.nix
+++ nix/systems/x86_64-linux/nixos-laptop/default.nix
@@ -15,6 +15,23 @@
 
   kde.enable = true;
   spicetify.enable = true;
+  fingerprint.enable = true;
+
+  nixpkgs.overlays = [
+    # Fingerprint reader needs fork
+    (_: prev: {
+      libfprint = prev.libfprint.overrideAttrs (oldAttrs: {
+        src = prev.fetchFromGitLab {
+          domain = "gitlab.freedesktop.org";
+          owner = "depau";
+          repo = "libfprint";
+          rev = "elanmoc2-working";
+          hash = "sha256-uYT1qQK5Hv4AcX9AT9jc36oygiOnpoVh7W4bdsiXWog=";
+        };
+        buildInputs = oldAttrs.buildInputs ++ [ prev.nss ];
+      });
+    })
+  ];
 
   onepassword = {
     enable = true;
