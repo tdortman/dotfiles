6687f099ab143c79d1e4a0a88971d6c46aed8e75
Tim Dortmann <git@tdortman.com>
2026-07-04 04:36:08 +0200

    chore(vencord): track nixpkgs master package
    Stop pinning Vencord 1.14.15 in the overlay and inherit the
    version from `final.master` instead.

--- nix/overlays/vencord/default.nix
+++ nix/overlays/vencord/default.nix
@@ -1,27 +1,27 @@
 _:
 
 final: prev: {
-  vencord = prev.vencord.overrideAttrs (
-    finalAttrs: oldAttrs: {
-      version = "1.14.15";
-      src = prev.fetchFromGitHub {
-        owner = "Vendicated";
-        repo = "Vencord";
-        rev = "v${finalAttrs.version}";
-        hash = "sha256-jQeLZa1rpKDkzWSpAqOa8snGRKLpv9xf9cwJ6hUwMzA=";
-      };
-      pnpmDeps = prev.fetchPnpmDeps {
-        inherit (finalAttrs)
-          pname
-          src
-          patches
-          postPatch
-          ;
-        pnpm = prev.pnpm_10;
-        fetcherVersion = 3;
-        hash = "sha256-hk1rnNog5xvuIVI0M1ZJ5xrEuk0zcBiYsbROUycdi+A=";
-      };
-    }
-  );
-  # inherit (final.master) vencord;
+  # vencord = prev.vencord.overrideAttrs (
+  #   finalAttrs: oldAttrs: {
+  #     version = "1.14.15";
+  #     src = prev.fetchFromGitHub {
+  #       owner = "Vendicated";
+  #       repo = "Vencord";
+  #       rev = "v${finalAttrs.version}";
+  #       hash = "sha256-jQeLZa1rpKDkzWSpAqOa8snGRKLpv9xf9cwJ6hUwMzA=";
+  #     };
+  #     pnpmDeps = prev.fetchPnpmDeps {
+  #       inherit (finalAttrs)
+  #         pname
+  #         src
+  #         patches
+  #         postPatch
+  #         ;
+  #       pnpm = prev.pnpm_10;
+  #       fetcherVersion = 3;
+  #       hash = "sha256-hk1rnNog5xvuIVI0M1ZJ5xrEuk0zcBiYsbROUycdi+A=";
+  #     };
+  #   }
+  # );
+  inherit (final.master) vencord;
 }
