0a645bb0acd7d1daa630740d54926b9448180333
Tim Dortmann <git@tdortman.com>
2026-07-12 03:13:51 +0200

    fix(nixos-vm): publish mdns names

--- nix/systems/x86_64-linux/nixos-vm/default.nix
+++ nix/systems/x86_64-linux/nixos-vm/default.nix
@@ -145,7 +145,12 @@
   services.avahi = {
     enable = true;
     openFirewall = true;
-    nssmdns4 = true;
+
+    publish = {
+      enable = true;
+      addresses = true;
+      workstation = true;
+    };
   };
 
   mime.librewolf.enable = true;
