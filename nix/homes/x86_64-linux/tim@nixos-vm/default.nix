35f866967946cc10b3da0f9fa20e93923817995a
Tim Dortmann <git@tdortman.com>
2026-07-12 03:12:55 +0200

    fix(nixos-vm): update plasma-manager settings

--- nix/homes/x86_64-linux/tim@nixos-vm/default.nix
+++ nix/homes/x86_64-linux/tim@nixos-vm/default.nix
@@ -17,8 +17,70 @@
       lookAndFeel = "org.kde.breezedark.desktop";
       wallpaper = "${pkgs.kdePackages.plasma-workspace-wallpapers}/share/wallpapers/MilkyWay/contents/images/5120x2880.png";
 
-      enableMiddleClickPaste = true;
+      enableMiddleClickPaste = false;
+
+      tooltipDelay = 5;
     };
+
+    panels = [
+      {
+        location = "bottom";
+        height = 48;
+        floating = true;
+        screen = "all";
+        hiding = "none";
+
+        widgets = [
+          { kickoff = { }; }
+          { pager = { }; }
+          {
+            iconTasks = {
+              launchers = [
+                "applications:org.kde.kdeconnect.app.desktop"
+                "applications:com.mitchellh.ghostty.desktop"
+                "applications:org.kde.dolphin.desktop"
+                "applications:librewolf.desktop"
+                "applications:discord.desktop"
+                "applications:spotify.desktop"
+              ];
+            };
+          }
+          "org.kde.plasma.marginsseparator"
+          {
+            systemTray = {
+              items = {
+                # Seems to not be functional at the moment, in fact the volume
+                # applet doesn't seem to exist at all?
+                #
+                # https://github.com/nix-community/plasma-manager/issues/565
+                configs."org.kde.plasma.volume".config.General.showVirtualDevices = true;
+              };
+            };
+          }
+          {
+            digitalClock = {
+              date = {
+                enable = true;
+                format.custom = "dd/MM/yyyy";
+                position = "belowTime";
+              };
+              time.format = "24h";
+              timeZone = {
+                selected = [
+                  "America/Los_Angeles"
+                  "Local"
+                  "Asia/Tokyo"
+                ];
+                lastSelected = "Local";
+              };
+              calendar.firstDayOfWeek = "monday";
+            };
+          }
+          "org.kde.plasma.showdesktop"
+        ];
+
+      }
+    ];
 
     kscreenlocker = {
       autoLock = false;
@@ -49,21 +111,21 @@
 
     fonts =
       let
-        inter = size: {
+        uiFont = size: {
           family = "Inter";
           pointSize = size;
         };
       in
       {
-        small = inter 8;
-        general = inter 10;
-        toolbar = inter 10;
-        menu = inter 10;
-        windowTitle = inter 10;
+        small = uiFont 9;
+        general = uiFont 11;
+        toolbar = uiFont 11;
+        menu = uiFont 11;
+        windowTitle = uiFont 11;
 
         fixedWidth = {
           family = "ComicCodeLigatures Nerd Font Mono";
-          pointSize = 10;
+          pointSize = 11;
         };
       };
 
@@ -78,14 +140,75 @@
       };
     };
 
+    powerdevil.AC = {
+      powerButtonAction = "hibernate";
+      autoSuspend.action = "nothing";
+      whenSleepingEnter = "standbyThenHibernate";
+      turnOffDisplay.idleTimeout = "never";
+      dimDisplay.enable = false;
+    };
+
     session.sessionRestore.restoreOpenApplicationsOnLogin = "startWithEmptySession";
 
     configFile = {
-      plasmashellrc."PlasmaViews/Panel 65/Defaults".thickness = 44;
+      kdeglobals.Sounds.Enable = false;
+      kdeglobals.General.TerminalApplication = "ghostty";
+      kdeglobals.General.TerminalService = "com.mitchellh.ghostty.desktop";
+
       kdeglobals.General.AccentColor = "#926ee4";
       baloofilerc."Basic Settings".Indexing-Enabled = false;
-    };
 
+      plasmanotifyrc.Notifications.PopupTimeout = 15000;
+
+      kcminputrc."Libinput/1133/16531/Logitech PRO X" = {
+        PointerAccelerationProfile = 1;
+        PointerAcceleration = 0.500;
+        ScrollFactor = 1;
+      };
+
+      kdeglobals.General = {
+        XftAntialias = true;
+        XftHintStyle = "hintslight";
+        XftSubPixel = "rgb";
+      };
+
+      kwinrc = {
+        # For some reason the workspace setting does not persist this setting
+        # so we write it directly into the config file (disable middle click paste)
+        Wayland.EnablePrimarySelection = false;
+
+        # Disable overview when moving to top left corner
+        "Effect-overview".BorderActivate = 9;
+      };
+
+      kded5rc.Module-browserintegrationreminder.autoload = false;
+      kded6rc.PlasmaBrowserIntegration.shownCount = 1;
+
+      kwinrulesrc = {
+        "28f2a5bd-b708-4cde-b12e-cc2e3bc1def1" = {
+          desktopfile = "/run/current-system/sw/share/applications/StreamController.desktop";
+          desktopfilerule = 4;
+        };
+
+        General = {
+          rules = "9f92402d-ab4d-45b7-9660-516c5f837c7b";
+          count = 1;
+        };
+
+        "9f92402d-ab4d-45b7-9660-516c5f837c7b" = {
+          Description = "GitButler maximize";
+          types = 1;
+          wmclass = "gitbutler-tauri";
+          wmclasscomplete = false;
+          wmclassmatch = 1;
+
+          maximizehoriz = true;
+          maximizehorizrule = 2;
+          maximizevert = true;
+          maximizevertrule = 2;
+        };
+      };
+    };
   };
 
   programs.konsole = {
