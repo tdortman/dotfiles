d9bae1b11a994a3b1bc864bd54e11702a4b6dbd3
Tim Dortmann <git@tdortman.com>
2026-07-14 22:06:59 +0200

    fix(nix): remove obsolete gaming overlay
    The gaming module locally overrides `openldap` solely for nixpkgs
    issue 513245. That workaround changes a package outside the gaming
    module concerns, so removing it lets the pinned package set provide
    the normal definition.

--- nix/modules/nixos/gaming/default.nix
+++ nix/modules/nixos/gaming/default.nix
@@ -24,15 +24,6 @@ in
 
     programs.gamescope.enable = true;
 
-    # FIXME: See https://github.com/NixOS/nixpkgs/issues/513245
-    nixpkgs.overlays = [
-      (_: prev: {
-        openldap = prev.openldap.overrideAttrs {
-          doCheck = !prev.stdenv.hostPlatform.isi686;
-        };
-      })
-    ];
-
     programs.gamemode.enable = true;
     environment.systemPackages = with pkgs; [
       wineWow64Packages.stableFull
