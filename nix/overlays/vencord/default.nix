7afecaf858133c3a5f3650051bac1f571fe0f602
Tim Dortmann <git@tdortman.com>
2026-07-28 12:04:33 +0200

    vencord: 1.14.15 -> 1.15.0

--- nix/overlays/vencord/default.nix
+++ nix/overlays/vencord/default.nix
@@ -3,13 +3,13 @@ _:
 final: prev: {
   vencord = (prev.master.vencord.override { pnpm_10 = prev.pnpm_11; }).overrideAttrs (
     finalAttrs: oldAttrs: {
-      version = "1.14.17";
+      version = "1.15.0";
 
       src = prev.fetchFromGitHub {
         owner = "Vendicated";
         repo = "Vencord";
-        rev = "4b9c27d905d6255141617546227a56073916ebd4";
-        hash = "sha256-dhBD/xlgf0VXVEP6I5kB6wmyJz7k8Epy1m8mHKhZuqs=";
+        rev = "v${finalAttrs.version}";
+        hash = "sha256-z3EY/nc9pHPjuMteY8ubYM3sqgjASznEG6B1U4mNCU4=";
       };
 
       patches = [ ./fix-deps.patch ];
