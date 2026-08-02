47fe8577b6e7a96fabe5c0bf6856fa3f1cbf191f
Tim Dortmann <git@tdortman.com>
2026-08-03 01:21:23 +0200

    fix(vencord): use upstream package
    Use `final.master.vencord` instead of the local pinned override.
    Avoid maintaining separate Vencord source and pnpm dependency hashes.

--- nix/overlays/vencord/default.nix
+++ nix/overlays/vencord/default.nix
@@ -1,37 +1,28 @@
 _:
 
 final: prev: {
-  vencord = (prev.master.vencord.override { pnpm_10 = prev.pnpm_11; }).overrideAttrs (
-    finalAttrs: oldAttrs: {
-      version = "1.15.0";
+  # vencord = prev.master.vencord.overrideAttrs (
+  #   finalAttrs: oldAttrs: {
+  #     version = "1.15.0";
 
-      src = prev.fetchFromGitHub {
-        owner = "Vendicated";
-        repo = "Vencord";
-        rev = "v${finalAttrs.version}";
-        hash = "sha256-z3EY/nc9pHPjuMteY8ubYM3sqgjASznEG6B1U4mNCU4=";
-      };
+  #     src = prev.fetchFromGitHub {
+  #       owner = "Vendicated";
+  #       repo = "Vencord";
+  #       rev = "v${finalAttrs.version}";
+  #       hash = "sha256-z3EY/nc9pHPjuMteY8ubYM3sqgjASznEG6B1U4mNCU4=";
+  #     };
 
-      patches = [ ./fix-deps.patch ];
+  #     pnpmDeps = prev.fetchPnpmDeps {
+  #       inherit (finalAttrs)
+  #         pname
+  #         src
+  #         ;
 
-      pnpmDeps = prev.fetchPnpmDeps {
-        inherit (finalAttrs)
-          patches
-          pname
-          postPatch
-          src
-          ;
-
-        fetcherVersion = 4;
-        hash = "sha256-JmTSfUVHsMG0TcOwXkZWinRxpONZagtwKzESd8Q4LlQ=";
-        pnpm = prev.pnpm_11;
-      };
-
-      postPatch = ''
-        substituteInPlace packages/vencord-types/package.json \
-          --replace-fail '"@types/react": "18.3.1"' '"@types/react": "19.1.0"'
-      '';
-    }
-  );
-  # inherit (final.master) vencord;
+  #       fetcherVersion = 4;
+  #       hash = "sha256-JmTSfUVHsMG0TcOwXkZWinRxpONZagtwKzESd8Q4LlQ=";
+  #       pnpm = prev.pnpm_11;
+  #     };
+  #   }
+  # );
+  inherit (final.master) vencord;
 }
