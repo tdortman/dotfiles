86b530a591c74a9c5f1d2c1e5b95dac3ea9c5048
Tim Dortmann <git@tdortman.com>
2026-08-21 23:05:44 +0200

    fix: force bluetooth hid handling to be handled by kernel

--- nix/systems/x86_64-linux/nixos-pc/default.nix
+++ nix/systems/x86_64-linux/nixos-pc/default.nix
@@ -259,6 +259,7 @@
   hardware = {
     bluetooth = {
       enable = true;
+      input.General.UserspaceHID = false;
       powerOnBoot = true;
       settings.General.Experimental = true;
     };
